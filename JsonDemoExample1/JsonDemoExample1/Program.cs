2d42b6e0d0df254c63ab912fa207efcb33615664
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-07 17:23:28 -0500

    Example for Parsing a JSON Object from string

---
+++ JsonDemoExample1/JsonDemoExample1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace JsonDemoExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parsing a JSON Object from string
+            string json = @"{
+                CPU: 'Intel',
+                Drives: [
+                    'DVD read/writer',
+                    '500 gigabyte hard drive'
+                ]
+            }";
+
+            JObject obj = JObject.Parse(json);
+            Console.WriteLine(obj);
+        }
+    }
+}
