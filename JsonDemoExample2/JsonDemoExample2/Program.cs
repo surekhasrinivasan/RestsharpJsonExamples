71de2a203d9d24a69c1e821e4c6d7732957dd136
surekhasrinivasan <surekhasrao@gmail.com>
2019-02-08 21:37:57 -0500

    Program to show serialize an object to json

---
+++ JsonDemoExample2/JsonDemoExample2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDemoExample2
+{
+    // Program to demo serialize and deserialize
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Serialize an object to JSON
+            Account account = new Account()
+            {
+                Email = "james@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string>
+                {
+                    "User",
+                    "Admin"
+                }
+            };
+
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+
+            Console.WriteLine(json);
+        }
+    }
+}
